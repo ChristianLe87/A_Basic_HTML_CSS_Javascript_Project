dd78df53909d151f76de74cbf2284e3060bacf2d
christian <christianlehnhoff@gmail.com>
2018-05-04 15:25:53 +0200

    Mas cambios

--- The_JS.js
+++ The_JS.js
@@ -1,3 +1,12 @@
 function Function_Hide_this() {
     $(".To_Hide_A_Button").hide();
 }
+
+
+function Function_Hide_this_2() {
+    $(".To_Hide_A_Button").hide();
+}
+
+function Function_Hide_this_3() {
+    $(".To_Hide_A_Button").hide();
+}
