20ed6d5ac0e26ffe602d528a07ff43656e23f71a
christianle87 <christianlehnhoff@gmail.com>
2018-05-06 21:12:08 +0200

    Mejora del proyecto

--- The_JS.js
+++ The_JS.js
@@ -1,12 +1,28 @@
+function Funcion_Alert() {
+  alert("Congratulations, \nyou just press a button!!!");
+  $(".To_Hide_A_Button").show();
+}
+
 function Function_Hide_this() {
-    $(".To_Hide_A_Button").hide();
+  $(".To_Hide_A_Button").hide();
 }
 
 
-function Function_Hide_this_2() {
-    $(".To_Hide_A_Button").hide();
+
+function previewFile() {
+  var preview = document.querySelector('img'); //selects the query named img
+  var file = document.querySelector('input[type=file]').files[0]; //sames as here
+  var reader = new FileReader();
+
+  reader.onloadend = function () {
+    preview.src = reader.result;
+  }
+
+  if (file) {
+    reader.readAsDataURL(file); //reads the data as a URL
+  } else {
+    preview.src = "";
+  }
 }
 
-function Function_Hide_this_3() {
-    $(".To_Hide_A_Button").hide();
-}
+previewFile();  //calls the function named previewFile()
